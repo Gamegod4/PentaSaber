9dd0d1f27ab1542b5842db3f67a2a9f4c45eb00b
Gamegod4 <51224222+Gamegod4@users.noreply.github.com>
2023-01-21 21:47:58 -0600

    Update GameNoteController_HandleCut.cs

--- PentaSaber/HarmonyPatches/GameNoteController_HandleCut.cs
+++ PentaSaber/HarmonyPatches/GameNoteController_HandleCut.cs
@@ -23,7 +23,7 @@ namespace PentaSaber.HarmonyPatches
         typeof(Quaternion),
         typeof(Vector3),
         typeof(bool)})]
-    public class GameNoteController_HandleCut
+    public class GameNoteController_NoteController_HandleCut
     {
         private static readonly MethodInfo _getBasicCutInfoMethod = typeof(NoteBasicCutInfoHelper)
             .GetMethod(nameof(NoteBasicCutInfoHelper.GetBasicCutInfo));
@@ -96,6 +96,91 @@ namespace PentaSaber.HarmonyPatches
             else
             {
                 Plugin.Log.Warn($"GameNoteController not in dictionary.");
+                return saberTypeOk;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BurstSliderGameNoteController), nameof(BurstSliderGameNoteController.HandleCut),
+        new Type[] { // List the Types of the method's parameters.
+        typeof(Saber),
+        typeof(Vector3),
+        typeof(Quaternion),
+        typeof(Vector3),
+        typeof(bool)})]
+    public class GameNoteController_BurstSlider_HandleCut
+    {
+        private static readonly MethodInfo _getBasicCutInfoMethod = typeof(NoteBasicCutInfoHelper)
+            .GetMethod(nameof(NoteBasicCutInfoHelper.GetBasicCutInfo));
+        private static readonly MethodInfo _cutIsGoodMethod = SymbolExtensions.GetMethodInfo(() => CutIsGood(null!, null!, false));
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            if (_getBasicCutInfoMethod == null)
+            {
+                Plugin.Log.Error($"_getBasicCutInfo is null");
+            }
+            bool finished = false;
+            bool readyToInsert = false;
+            //Plugin.Log.Critical($"Starting Transpile");
+            foreach (var code in instructions)
+            {
+                //Plugin.Log.Info($"   {code}");
+                if (!finished)
+                {
+                    if (!readyToInsert)
+                    {
+                        if (code.opcode == OpCodes.Call && code.Calls(_getBasicCutInfoMethod))
+                        {
+                            //Plugin.Log.Critical($"ReadyToInsert");
+                            readyToInsert = true;
+                        }
+                    }
+                    else
+                    {
+                        //Plugin.Log.Critical($"Inserting OpCodes");
+                        yield return new CodeInstruction(OpCodes.Ldarg_0); // Load GameNoteController (this)
+                        yield return new CodeInstruction(OpCodes.Ldarg_1); // Load Argument 1 (Saber)
+                        yield return new CodeInstruction(OpCodes.Ldloc_3); // Load flag3 (saberTypeOk)
+                        yield return new CodeInstruction(OpCodes.Call, _cutIsGoodMethod);
+                        yield return new CodeInstruction(OpCodes.Stloc_3); // Store result in flag3 (saberTypeOk)
+                        finished = true;
+                    }
+                }
+                yield return code;
+            }
+        }
+
+        static bool CutIsGood(BurstSliderGameNoteController gameNote, Saber saber, bool saberTypeOk)
+        {
+            //Plugin.Log.Debug($"Executing CutIsGood: {gameNote?.GetInstanceID()} | {saber?.name} | {saber?.saberType} | {saberTypeOk}");
+            PentaSaberController? controller = PentaSaberController.Instance;
+            if (gameNote == null)
+            {
+                Plugin.Log.Warn($"BurstSliderGameNoteController is null.");
+                return saberTypeOk;
+            }
+            if (saber == null)
+            {
+                Plugin.Log.Warn($"Saber is null.");
+                return saberTypeOk;
+            }
+            if (controller == null)
+            {
+                Plugin.Log.Warn($"PentaSaberController is null.");
+                return saberTypeOk;
+            }
+            if (controller.TryGetNoteType(gameNote, out var pentaNoteType))
+            {
+                PentaNoteType pentaSaberType = controller.GetCurrentSaberTypeBySaber(saber.GetInstanceID());
+                bool isGood = pentaNoteType == pentaSaberType
+                    || pentaNoteType == PentaNoteType.Neutral || pentaNoteType == PentaNoteType.Neutral2;
+                //Plugin.Log.Info($"Cutting note '{gameNote.GetInstanceID()}' ({pentaNoteType}) with saber '{pentaSaberType}' aka '{saber}':'{saber.GetInstanceID()}' - Good: {isGood}");
+                return isGood;
+            }
+            else
+            {
+                Plugin.Log.Warn($"BurstSliderGameNoteController not in dictionary.");
                 return saberTypeOk;
             }
         }
