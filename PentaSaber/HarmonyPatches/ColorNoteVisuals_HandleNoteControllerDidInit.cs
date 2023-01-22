452ab9078c1c85043c8f051f52d0e4a2ea1d288c
Gamegod4 <51224222+Gamegod4@users.noreply.github.com>
2023-01-21 21:47:37 -0600

    Update ColorNoteVisuals_HandleNoteControllerDidInit.cs

--- PentaSaber/HarmonyPatches/ColorNoteVisuals_HandleNoteControllerDidInit.cs
+++ PentaSaber/HarmonyPatches/ColorNoteVisuals_HandleNoteControllerDidInit.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using IPA.Utilities;
 using System;
 using System.Collections.Generic;
@@ -17,13 +17,12 @@ namespace PentaSaber.HarmonyPatches
     [HarmonyPatch(typeof(ColorNoteVisuals), nameof(ColorNoteVisuals.HandleNoteControllerDidInit),
         new Type[] { // List the Types of the method's parameters.
         typeof(NoteControllerBase) })]
-    public class ColorNoteVisuals_HandleNoteControllerDidInit
+    public class ColorNoteVisuals_NoteController_HandleNoteControllerDidInit
     {
         /// <summary>
         /// This code is run before the original code in MethodToPatch is run.
         /// </summary>
-        static void Postfix(ColorNoteVisuals __instance, ref NoteControllerBase noteController,
-            MaterialPropertyBlockController[] ____materialPropertyBlockControllers, ref Color ____noteColor)
+        static void Postfix(ColorNoteVisuals __instance, ref NoteControllerBase noteController, MaterialPropertyBlockController[] ____materialPropertyBlockControllers, ref Color ____noteColor)
         {
             if (!(noteController is GameNoteController gnc))
                 return;
@@ -33,6 +32,29 @@ namespace PentaSaber.HarmonyPatches
             PentaNoteType pentaNoteType = pc.SetNoteType(gnc);
             ____noteColor = Plugin.Config.GetColor(pentaNoteType);
             //Plugin.Log.Info($"Setting note '{gnc.GetInstanceID()}' to {pentaNoteType}");
+            foreach (var m in ____materialPropertyBlockControllers)
+            {
+                m.materialPropertyBlock.SetColor(Shader.PropertyToID("_Color"), ____noteColor);
+                m.ApplyChanges();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ColorNoteVisuals), nameof(ColorNoteVisuals.HandleNoteControllerDidInit),
+        new Type[] { // List the Types of the method's parameters.
+        typeof(BurstSliderGameNoteController) })]
+    public class ColorNoteVisuals_BurstSlider_HandleNoteControllerDidInit
+    {
+        static void Postfix(ColorNoteVisuals __instance, ref BurstSliderGameNoteController noteController, MaterialPropertyBlockController[] ____materialPropertyBlockControllers, ref Color ____noteColor)
+        {
+            if (!(noteController is BurstSliderGameNoteController bnc))
+                return;
+            var pc = PentaSaberController.Instance;
+            if (pc == null)
+                return;
+            PentaNoteType pentaNoteType = pc.SetNoteType(bnc);
+            ____noteColor = Plugin.Config.GetColor(pentaNoteType);
+            //Plugin.Log.Info($"Setting note '{bnc.GetInstanceID()}' to {pentaNoteType}");
             foreach (var m in ____materialPropertyBlockControllers)
             {
                 m.materialPropertyBlock.SetColor(Shader.PropertyToID("_Color"), ____noteColor);
